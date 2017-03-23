fe203e7064d3479767bff4d8076d7a096fb0ef2d
imasih <nilgh91@gmail.com>
2017-03-23 19:23:07 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -1,15 +1,15 @@
-bot = dofile('/home/username/inline/data/utils.lua')
-json = dofile('/home/username/inline/data/JSON.lua')
+bot = dofile('/home/bot/inline/data/utils.lua')
+json = dofile('/home/bot/inline/data/JSON.lua')
 URL = require "socket.url"
 serpent = require("serpent")
 http = require "socket.http"
 https = require "ssl.https"
 redis = require('redis')
 db = redis.connect('127.0.0.1', 6379)
-BASE = '/home/username/inline/bot/'
-SUDO = 304107094 --sudo id
-sudo_users = {304107094,123456789,Userid}
-BOTS = 00000000 --bot id
+BASE = '/home/bot/inline/bot/'
+SUDO = 326824638 --sudo id
+sudo_users = {326824638,123456789,Userid}
+BOTS = 25940597 --bot id
 bot_id = db:get(SUDO..'bot_id')
 function vardump(value)
   print(serpent.block(value, {comment=false}))
@@ -875,7 +875,7 @@ end
             end
           tdcli_function({
       ID = "GetInlineQueryResults",
-      bot_user_id_ = 254778917,
+      bot_user_id_ = 261919398,
       chat_id_ = msg.chat_id_,
       user_location_ = {
         ID = "Location",
